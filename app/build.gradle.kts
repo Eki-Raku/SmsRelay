2a5c77a008c39730c8bf0c649ee8f70fcad1de46
Eki-Raku <46719843+Eki-Raku@users.noreply.github.com>
2026-09-07 15:06:24 +0800

    chore: prepare 0.5.1 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.raku.smsrelay"
         minSdk = 26
         targetSdk = 37
-        versionCode = 7
-        versionName = "0.5.0"
+        versionCode = 8
+        versionName = "0.5.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
